1d1c19bb1ca429cb6e2a43b94b76eba8e1c15699
Lyon.Xu <lyon_it@163.com>
2018-03-09 16:23:08 +0800

    [add] A category of NSString about security.

---
+++ LXKit/Category/Foundation/NSString+LXSecurity.m
@@ -0,0 +1,29 @@
+//
+//  NSString+LXSecurity.m
+//  LXKit
+//
+//  Created by 徐良华 on 2018/3/9.
+//  Copyright © 2018年 Lyon. All rights reserved.
+//
+
+#import "NSString+LXSecurity.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString (LXSecurity)
+
++ (nullable NSString *)fileNameMD5ForKey:(nullable NSString *)key {
+    const char *str = key.UTF8String;
+    if (str == NULL) {
+        str = "";
+    }
+    unsigned char r[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(str, (CC_LONG)strlen(str), r);
+    NSURL *keyURL = [NSURL URLWithString:key];
+    NSString *ext = keyURL ? keyURL.pathExtension : key.pathExtension;
+    NSString *filename = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%@",
+                          r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10],
+                          r[11], r[12], r[13], r[14], r[15], ext.length == 0 ? @"" : [NSString stringWithFormat:@".%@", ext]];
+    return filename;
+}
+
+@end
